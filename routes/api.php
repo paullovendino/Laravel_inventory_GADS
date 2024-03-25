3ae537b9399eae5dde7bff440aef0dde033571f4
Kitsus_Amogus <118980466+256210KitsuuuuHiddenOnGitHub@users.noreply.github.com>
2024-03-25 14:54:49 +0800

    Revert "InventoryController - Create/Update "

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,4 @@ Route::prefix('/inventory')->group(function(){
     Route::get('/index', [InventoryController::class, 'index']);
     Route::post('/delete', [InventoryController::class, 'delete']);
     Route::post('/search', [InventoryController::class, 'search']);
-    Route::post('/create', [InventoryController::class, 'create']);
-    Route::post('/update', [InventoryController::class, 'update']);
 });
