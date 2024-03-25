49cd2eb25dd94fc528bc08b309201d34c52017a3
Exceil-Carpio <exceilmatthew.carpio@gmail.com>
2024-03-25 14:38:25 +0800

    Updated Search and FIlter

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -72,11 +72,10 @@ class InventoryController extends Controller
     }
 
     public function search(Request $request){
-        $data = products_table::where('id', $request->input('id'))
-        ->orWhere('Model_Name', $request->input('modelName'))
-        ->orWhere('Category', $request->input('category'))
-        ->orWhere('Department_assigned_item_location', $request->input('department'))
-        ->orWhere('SKU_Serial_Number', $request->input('sku'))
+        $data = products_table::where('Model_Name', 'ilike', '%'.$request->input('search').'%')
+        ->orWhere('Category', 'ilike', '%'.$request->input('search').'%')
+        ->orWhere('Department_assigned_item_location', 'ilike', '%'.$request->input('search').'%')
+        ->orWhere('SKU_Serial_Number', 'ilike', '%'.$request->input('search').'%')
         ->get();
 
         return response()->json([
@@ -87,7 +86,52 @@ class InventoryController extends Controller
         ], 200);
     }
 
-    public function filter(Request $request){ //waiting for frontend
-        
+    public function filter(Request $request){
+        $data = products_table::where('Model_Name', '<>', '');
+
+        $modelName = $request->input('modelName');
+        $category = $request->input('category');
+        $department = $request->input('department');
+        // $expectedQuantity = $request->input('expectedQuantity');
+        // $actualQuantity = $request->input('actualQuantity');
+        $dateArrived = $request->input('dateArrived');
+        $dateChecked = $request->input('dateChecked');
+
+        if(isset($modelName)){
+            $data->where('Model_Name', $modelName);
+        }
+
+        if(isset($category)){
+            $data->where('Category', $category);
+        }
+
+        if(isset($department)){
+            $data->where('Department_assigned_item_location', 'ilike', '%'.$department.'%');
+        }
+
+        //Needs call from frontend
+
+        // if(isset($expectedQuantity)){
+        //     $data->where('expected_Quantity', $expectedQuantity);
+        // }
+
+        // if(isset($actualQuantity)){
+        //     $data->where('actual_Quantity', $actualQuantity);
+        // }
+
+        if(isset($dateArrived)){
+            $data->where('Date_Arrived_Listed_Date', $dateArrived);
+        }
+
+        if(isset($dateChecked)){
+            $data->where('Date_Checked', $dateChecked);
+        }
+
+        return response()->json([
+            'status' => true,
+            'code' => 200,
+            'error' => null,
+            'data' => $data->get(),
+        ], 200);
     }
 }
